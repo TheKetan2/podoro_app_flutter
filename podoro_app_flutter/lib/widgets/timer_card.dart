3eb28c341364a40214250b052634dbaf62dac00f
Ketan Ramteke <Ketantoo@gmail.com>
2022-09-20 22:57:45 +0530

    added timerOption cards

--- podoro_app_flutter/lib/widgets/timer_card.dart
+++ podoro_app_flutter/lib/widgets/timer_card.dart
@@ -52,7 +52,7 @@ class TimerCard extends StatelessWidget {
             color: Colors.grey.withOpacity(0.5),
             spreadRadius: 4,
             blurRadius: 4,
-            offset: Offset(0, 2),
+            offset: const Offset(0, 2),
           )
         ],
       ),
